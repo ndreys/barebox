40412c23b0f72510f1d38c8a37b4bc8b30157b82
Andrey Gusakov <andrey.gusakov@cogentembedded.com>
2015-12-15 15:14:57 +0300

    Zodiac: PIC fix build fail when CMD_ZODIAC_PIC not enabled

--- arch/arm/boards/zodiac/board.c
+++ arch/arm/boards/zodiac/board.c
@@ -184,6 +184,8 @@ static int imx51_zodiac_init(void)
 }
 coredevice_initcall(imx51_zodiac_init);
 
+#ifdef CONFIG_CMD_ZODIAC_PIC
+
 extern void pic_uart_init(struct console_device *cdev, int speed);
 
 static int imx51_zodiac_lateinit(void)
@@ -200,6 +202,8 @@ static int imx51_zodiac_lateinit(void)
 	return 0;
 }
 postenvironment_initcall(imx51_zodiac_lateinit);
+
+#endif
 
 #ifdef CONFIG_ARCH_IMX_XLOAD
 
