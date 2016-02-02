7c6be941604cb147fb781f7ea5c7b630324850cf
Andrey Gusakov <andrey.gusakov@cogentembedded.com>
2016-02-02 11:16:30 +0300

    PIC: add CMD_LCD_TYPE command

--- commands/pic.c
+++ commands/pic.c
@@ -1228,6 +1228,52 @@ BAREBOX_CMD_START(z_lcd_stable)
 	BAREBOX_CMD_GROUP(CMD_GRP_MISC)
 BAREBOX_CMD_END
 
+/* EEPROM-related commands */
+
+int do_z_get_lcd(int argc, char *argv[])
+{
+	int ret;
+	uint8_t data[1];
+
+	if ((!pic) || (!pic->cdev))
+		return -ENODEV;
+	if (argc != 1)
+		return COMMAND_ERROR_USAGE;
+
+	ret = zii_eeprom_read(pic, ZII_PIC_EEPROM_RDU, data, 0x0050, 1);
+	if (ret != 1)
+		return ret;
+
+	switch(data[0]) {
+	case 0x01:
+		printf("10.1\"\n");
+	break;
+	case 0x02:
+		printf("12.1\"\n");
+	break;
+	case 0x03:
+		printf("11.6\"\n");
+	break;
+	case 0x04:
+		printf("13.3\"\n");
+	break;
+	case 0x05:
+		printf("18.5\"\n");
+	break;
+	default:
+		printf("UNKNOWN: 0x%02x\n", data[0]);
+	break;
+	}
+
+	return 0;
+}
+
+BAREBOX_CMD_START(z_get_lcd)
+	.cmd		= do_z_get_lcd,
+	BAREBOX_CMD_DESC("Get LCD type")
+	BAREBOX_CMD_GROUP(CMD_GRP_MISC)
+BAREBOX_CMD_END
+
 /* IMS: Add U-Boot commands to Pet the Watchdog Timer using the Microchip PIC */
 int do_pic_pet_wdt (int argc, char *argv[])
 {
