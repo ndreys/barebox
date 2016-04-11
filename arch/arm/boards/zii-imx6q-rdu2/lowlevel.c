0d8bb23a749c0f49c8a70af1e4722f57394a2978
Andrey Smirnov <andrew.smirnov@gmail.com>
2016-04-10 21:56:45 -0700

    RDU2+: Add separate device tree for MX6+ variant
    Add separate device tree for MX6+ variant so that it would be possible
    to enable MX6+ specific functionality in PCIe driver.
    
    For now this commit is less than ideal because it just make the PCIe
    compatiblity change as a part of board's .dts file. The proper way to
    fix this would be to include "imx6qp.dtsi", however this file is not
    going to be avialible until upstream Barebox syncs up "dts" directory
    with upstream Kernel.

--- arch/arm/boards/zii-imx6q-rdu2/lowlevel.c
+++ arch/arm/boards/zii-imx6q-rdu2/lowlevel.c
@@ -39,6 +39,8 @@ ENTRY_FUNCTION(start_imx6q_zii_rdu2, r0, r1, r2)
 	barebox_arm_entry(0x10000000, SZ_2G, fdt);
 }
 
+extern char __dtb_imx6qp_zii_rdu2_start[];
+
 ENTRY_FUNCTION(start_imx6qp_zii_rdu2, r0, r1, r2)
 {
 	void *fdt;
@@ -48,7 +50,7 @@ ENTRY_FUNCTION(start_imx6qp_zii_rdu2, r0, r1, r2)
 	if (IS_ENABLED(CONFIG_DEBUG_LL))
 		setup_uart();
 
-	fdt = __dtb_imx6q_zii_rdu2_start - get_runtime_offset();
+	fdt = __dtb_imx6qp_zii_rdu2_start - get_runtime_offset();
 
 	barebox_arm_entry(0x10000000, SZ_2G, fdt);
 }
