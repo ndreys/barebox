d15571706750263389e323c07fbc28847d148dc2
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-02-11 07:43:45 -0800

    ARM: rdu2: Try to fetch IP/Netmask info from RAVE SP
    TODO: Better message
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>

--- arch/arm/boards/zii-imx6q-rdu2/board.c
+++ arch/arm/boards/zii-imx6q-rdu2/board.c
@@ -21,6 +21,7 @@
 #include <mach/bbu.h>
 #include <mach/imx6.h>
 #include <net.h>
+#include <globalvar.h>
 #include <linux/nvmem-consumer.h>
 
 #define RDU2_DAC1_RESET	IMX_GPIO_NR(1, 0)
@@ -226,7 +227,14 @@ static int rdu2_eth_register_ethaddr(struct device_node *np)
 
 static int rdu2_ethernet0_init(void)
 {
+	static char *rdu_networkconfig;
 	struct device_node *np, *root;
+	IPaddr_t ip_address, netmask;
+	struct {
+		__le32 ip_address;
+		__le32 netmask;
+	} *configuration;
+	int ret;
 
 	if (!of_machine_is_compatible("zii,imx6q-zii-rdu2") &&
 	    !of_machine_is_compatible("zii,imx6qp-zii-rdu2"))
@@ -237,7 +245,36 @@ static int rdu2_ethernet0_init(void)
 	if (!np)
 		return -ENODEV;
 
-	return rdu2_eth_register_ethaddr(np);
+	ret = rdu2_eth_register_ethaddr(np);
+	if (ret)
+		pr_warn("%s: Failed to register MAC address\n",
+			np->name);
+
+	configuration = rdu2_nvmem_cell_read(np, "ip-address",
+					     sizeof(*configuration));
+	if (IS_ERR(configuration)) {
+		pr_err("%s: Failed to read IP address\n", np->name);
+		return PTR_ERR(configuration);
+	}
+
+	ip_address = le32_to_cpu(configuration->ip_address);
+	netmask    = le32_to_cpu(configuration->netmask);
+	free(configuration);
+
+	pr_info("%s: %pI4/%pI4\n", np->name, &ip_address, &netmask);
+
+	if (ip_address) {
+		setenv_ip("ipaddr",  ip_address);
+		setenv_ip("netmask", netmask);
+		rdu_networkconfig = basprintf("ip=%pI4:::%pI4::eth0:",
+					      &ip_address, &netmask);
+	} else {
+		rdu_networkconfig = xstrdup("ip=dhcp");
+	}
+
+	globalvar_add_simple_string("linux.bootargs.rdu_network", &rdu_networkconfig);
+
+	return 0;
 }
 late_initcall(rdu2_ethernet0_init);
 
