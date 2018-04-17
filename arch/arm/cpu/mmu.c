67b4291230cf259f507e469fee423911e2014937
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-04-16 20:48:30 -0700

    ARM: mmu: Make use of SZ_1M in map_io_sections90
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>

--- arch/arm/cpu/mmu.c
+++ arch/arm/cpu/mmu.c
@@ -196,7 +196,7 @@ void *map_io_sections(unsigned long phys, void *_start, size_t size)
 {
 	unsigned long start = (unsigned long)_start, sec;
 
-	for (sec = start; sec < start + size; sec += (1 << 20), phys += SZ_1M)
+	for (sec = start; sec < start + size; sec += SZ_1M, phys += SZ_1M)
 		ttb[sec >> 20] = phys | PMD_SECT_DEF_UNCACHED;
 
 	dma_flush_range((unsigned long)ttb, (unsigned long)ttb + 0x4000);
