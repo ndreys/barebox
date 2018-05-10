66cb01386cad7e5ce4f2aad785cf4bf034284e37
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-05-09 17:22:07 -0700

    scripts/imx: Add trivial i.MX8 support
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>

--- scripts/imx/imx.c
+++ scripts/imx/imx.c
@@ -233,6 +233,7 @@ static struct soc_type socs[] = {
 	{ .name = "imx6", .header_version = 2, .cpu_type = IMX_CPU_IMX6 },
 	{ .name = "imx7", .header_version = 2, .cpu_type = IMX_CPU_IMX7 },
 	{ .name = "vf610", .header_version = 2, .cpu_type = IMX_CPU_VF610 },
+	{ .name = "imx8", .header_version = 2, .cpu_type = IMX_CPU_IMX8MQ },
 };
 
 static int do_soc(struct config_data *data, int argc, char *argv[])
