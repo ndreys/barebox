1a9ee8b3a5385bde9b02f08c77941a938b18ed70
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-05-16 23:19:13 -0700

    ARM: i.MX: bbu: Add support for SPI/I2C on VFxxx
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>

--- arch/arm/mach-imx/include/mach/bbu.h
+++ arch/arm/mach-imx/include/mach/bbu.h
@@ -33,6 +33,9 @@ int imx6_bbu_internal_spi_i2c_register_handler(const char *name, char *devicefil
 int vf610_bbu_internal_mmc_register_handler(const char *name, char *devicefile,
 		unsigned long flags);
 
+int vf610_bbu_internal_spi_i2c_register_handler(const char *name, char *devicefile,
+						unsigned long flags);
+
 int imx_bbu_external_nor_register_handler(const char *name, char *devicefile,
 		unsigned long flags);
 
@@ -92,6 +95,14 @@ static inline int imx_bbu_external_nor_register_handler(const char *name, char *
 {
 	return -ENOSYS;
 }
+
+static inline int
+vf610_bbu_internal_spi_i2c_register_handler(const char *name, char *devicefile,
+					    unsigned long flags)
+{
+	return -ENOSYS;
+}
+
 #endif
 
 #if defined(CONFIG_BAREBOX_UPDATE_IMX_EXTERNAL_NAND)
