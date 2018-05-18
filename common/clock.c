090c5086fc5740c6d49c17347d47c8002623d1eb
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-05-17 22:12:43 -0700

    common/clock: Move delay and timeout functions to clock.h
    Move delay and timeout functions to clock.h and add necessary
    preprocessor code in order to make them availible in PBL. The header
    is written such that by providing a pre-define get_time_ns
    macro (before the inclusion point of clock.h) user can make use of all
    of the timeout/delay functionality without having to have a
    functioning clock source.
    
    An example of simple use-case would be using any variant of
    readx_poll_timeout with timeout of zero (infinite timeout) by
    providing the following trivial definition:
    
    \#define get_time_ns()	0
    
    before <linux/iopoll.h> is included.
    
    More sophisticated use-case would be providing a definition for a very
    simplistic get_time_ns() function and using actual timekeeping
    functionality such as is_timeout() or udelay().
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>

--- common/clock.c
+++ common/clock.c
@@ -25,7 +25,6 @@
 #include <init.h>
 #include <asm-generic/div64.h>
 #include <clock.h>
-#include <poller.h>
 
 static uint64_t time_ns;
 
@@ -169,57 +168,6 @@ uint32_t clocksource_hz2mult(uint32_t hz, uint32_t shift_constant)
 
 	return (uint32_t)tmp;
 }
-
-int is_timeout_non_interruptible(uint64_t start_ns, uint64_t time_offset_ns)
-{
-	if ((int64_t)(start_ns + time_offset_ns - get_time_ns()) < 0)
-		return 1;
-	else
-		return 0;
-}
-EXPORT_SYMBOL(is_timeout_non_interruptible);
-
-int is_timeout(uint64_t start_ns, uint64_t time_offset_ns)
-{
-	if (time_offset_ns >= 100 * USECOND)
-		poller_call();
-
-	return is_timeout_non_interruptible(start_ns, time_offset_ns);
-}
-EXPORT_SYMBOL(is_timeout);
-
-void ndelay(unsigned long nsecs)
-{
-	uint64_t start = get_time_ns();
-
-	while(!is_timeout_non_interruptible(start, nsecs));
-}
-EXPORT_SYMBOL(ndelay);
-
-void udelay(unsigned long usecs)
-{
-	uint64_t start = get_time_ns();
-
-	while(!is_timeout(start, usecs * USECOND));
-}
-EXPORT_SYMBOL(udelay);
-
-void mdelay(unsigned long msecs)
-{
-	uint64_t start = get_time_ns();
-
-	while(!is_timeout(start, msecs * MSECOND));
-}
-EXPORT_SYMBOL(mdelay);
-
-void mdelay_non_interruptible(unsigned long msecs)
-{
-	uint64_t start = get_time_ns();
-
-	while (!is_timeout_non_interruptible(start, msecs * MSECOND))
-		;
-}
-EXPORT_SYMBOL(mdelay_non_interruptible);
 
 int init_clock(struct clocksource *cs)
 {
