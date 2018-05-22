578c554e6429002ef1394964b5ba4f3af5f028c6
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-05-21 19:49:35 -0700

    ARM: i.MX8: Add trivial imx8-gpc.h
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>

---
+++ arch/arm/mach-imx/include/mach/imx8-gpc.h
@@ -0,0 +1,10 @@
+#ifndef __IMX8_GPC_H__
+#define __IMX8_GPC_H__
+
+#define GPC_PGC_CPU_0_1_MAPPING	0x00ec
+#define DDR1_A53_DOMAIN		BIT(7)
+
+#define GPC_PU_PGC_SW_PUP_REQ	0x00f8
+#define DDR1_SW_PUP_REQ		BIT(5)
+
+#endif
