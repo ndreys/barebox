886baf48fe05cfa71f11e259ba7f7c420721cecf
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-06-22 09:23:58 -0700

    ARM: rdu1: Create switch-eeprom link for RAVE SP attached devices
    Some RDU1s come with docks where switch EEPROM is connected to RAVE SP
    device via an I2C mux. In order to create a consistent interface to
    access that EEPROM on such devices, add a p/n fixup that would create
    an appropriate link in devfs.
    
    Devices where said EEPROM is accessible via MDIO bus are expected to
    be handled via a separate p/n fixup as well.
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>

--- arch/arm/boards/zii-imx51-rdu1/board.c
+++ arch/arm/boards/zii-imx51-rdu1/board.c
@@ -21,6 +21,8 @@
 #include <libfile.h>
 #include <mach/imx5.h>
 
+#include "pn-fixup.h"
+
 static int zii_rdu1_init(void)
 {
 	if (!of_machine_is_compatible("zii,imx51-rdu1"))
@@ -39,3 +41,42 @@ static int zii_rdu1_init(void)
 	return 0;
 }
 coredevice_initcall(zii_rdu1_init);
+
+static void zii_rdu1_sp_switch_eeprom(void)
+{
+	struct device_node *np, *root;
+	struct cdev *cdev;
+
+	root = of_get_root_node();
+	np   = of_find_node_by_name(root, "eeprom@ae");
+	if (WARN_ON(!np))
+		return;
+
+	cdev = cdev_by_device_node(np);
+	if (!cdev) {
+		pr_err("Couldn't find switch eeprom\n");
+		return;
+	}
+
+	WARN_ON(devfs_create_link(cdev, "switch-eeprom"));
+}
+
+static const struct zii_pn_fixup zii_rdu1_dds_fixups[] = {
+	{ "05-0004-02-E3", zii_rdu1_sp_switch_eeprom }
+};
+
+static int zii_rdu1_process_fixups(void)
+{
+	int ret;
+
+	if (!of_machine_is_compatible("zii,imx51-rdu1"))
+		return 0;
+
+	ret = zii_process_dds_fixups(ARRAY_AND_SIZE(zii_rdu1_dds_fixups));
+	/*
+	 * TODO: Add RDU p/n fixups
+	 */
+
+	return ret;
+}
+postmmu_initcall(zii_rdu1_process_fixups);
