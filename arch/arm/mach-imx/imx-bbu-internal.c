22acd0f7bd7b152c43d07c2d0bb799755afea08e
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-06-25 19:55:16 -0700

    ARM: i.MX: bbu: Simplify imx53_bbu_internal_nand_register_handler()
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>

--- arch/arm/mach-imx/imx-bbu-internal.c
+++ arch/arm/mach-imx/imx-bbu-internal.c
@@ -495,12 +495,11 @@ int imx53_bbu_internal_nand_register_handler(const char *name,
 {
 	struct imx_internal_bbu_handler *imx_handler;
 
-	imx_handler = __init_handler(name, NULL, flags |
+	imx_handler = __init_handler(name, "/dev/nand0", flags |
 				     IMX_INTERNAL_FLAG_NAND);
 	imx_handler->flash_header_offset = FLASH_HEADER_OFFSET_MMC;
 
 	imx_handler->expected_type = filetype_imx_image_v2;
-	imx_handler->handler.devicefile = "/dev/nand0";
 	imx_handler->device_size = partition_size;
 
 	return __register_handler(imx_handler);
