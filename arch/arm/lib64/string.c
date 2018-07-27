445e0687594912d576b9a036155c05fcbe24dbca
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-07-26 20:16:39 -0700

    ARM: lib64: Properly handle unaligned addresses in string functions
    Together FEC driver and parts of IP stack might end up trying to
    memcpy() small chunks of memory from uncached (that is Device memory)
    addresses that are not properly aligned, leading to data abort.
    
    To prevent such cases, add code to guard unaligned accesses and
    redirect them to byte-wise implementations which do not have the above
    problem.
    
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>

--- arch/arm/lib64/string.c
+++ arch/arm/lib64/string.c
@@ -5,9 +5,16 @@
 void *__arch_memset(void *dst, int c, __kernel_size_t size);
 void *__arch_memcpy(void * dest, const void *src, size_t count);
 
+static bool properly_aligned(const void *ptr)
+{
+	const unsigned long address = (unsigned long)ptr;
+
+	return get_cr() & CR_M && IS_ALIGNED(address, 16);
+}
+
 void *memset(void *dst, int c, __kernel_size_t size)
 {
-	if (likely(get_cr() & CR_M))
+	if (likely(properly_aligned(dst)))
 		return __arch_memset(dst, c, size);
 
 	return __default_memset(dst, c, size);
@@ -15,7 +22,7 @@ void *memset(void *dst, int c, __kernel_size_t size)
 
 void *memcpy(void * dest, const void *src, size_t count)
 {
-	if (likely(get_cr() & CR_M))
+	if (likely(properly_aligned(dest) && properly_aligned(src)))
 		return __arch_memcpy(dest, src, count);
 
 	return __default_memcpy(dest, src, count);
